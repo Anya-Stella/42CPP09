3c2bf386c663e5c4e2f8d1fa7c9c0534d0ffdbb3
Tsubasa Ishihara <tishihar@c6r2s8.42tokyo.jp>
2026-03-11 16:50:14 +0900

    init

---
+++ ex00/src/main.cpp
@@ -0,0 +1,26 @@
+#include "../include/BitcoinExchange.hpp"
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	// validation
+	if (argc != 2)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
+
+	// generate obj(To store DB value in a map structure)
+	// read DB
+	// output
+	try
+	{
+		
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
